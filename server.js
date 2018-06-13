9e0ca154f3f68480a2f2eb2fba35ee083fb974f1
Daniel c <bitclaw@gmail.com>
2018-06-13 12:29:49 -0500

    Add .dotenv

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const dotenv = require('dontenv').config({path: 'variables.env'});
+const dotenv = require('dotenv').config({path: 'variables.env'});
 const logger = require('./logger')
 
 logger.info(`USER = ${process.env.USER}`)
